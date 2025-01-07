5f55002b805935e03d453cb7e64730751b075a17
abhays72 <abhaysudhir@icloud.com>
2025-01-06 17:48:15 -0800

    prompt eng 3 dont say based on provided email

--- backend_scripts/ask_gpt.py
+++ backend_scripts/ask_gpt.py
@@ -84,7 +84,7 @@ def ask_gpt(contexts, question):
     token_limit = 12289  # Token limit for context
     prompt_start = """
     Answer the question based on the context below. I want whatever you say to be nice, respectful and helpful and align with scouting principles. 
-    Give your answers in markdown format.
+    Give your answers in markdown format and don't say based on provided email context or anything like that.
     You are a highly specialized assistant designed to answer questions about Boy Scouts of America (BSA) programs, policies, activities, and procedures. You will be provided with the context of an email (or other documentation) and a user question. The user is either a boy scout or an adult leader.
     Your task is to:
 
