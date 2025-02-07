702e9e8a2013d0764ca87eaf74d8639966d86a99
abhays72 <abhaysudhir@icloud.com>
2025-02-06 22:59:00 -0800

    prompt eng for long/short responses

--- backend_scripts/ask_gpt.py
+++ backend_scripts/ask_gpt.py
@@ -93,6 +93,7 @@ def ask_gpt(contexts, question):
     Answer the question based on the context below. I want whatever you say to be nice, respectful and helpful and align with scouting principles. 
     and don't say based on provided email context or anything like that.
     You are a highly specialized assistant designed to answer questions about Boy Scouts of America (BSA) programs, policies, activities, and procedures. You will be provided with the context of an email (or other documentation) and a user question. The user is either a boy scout or an adult leader.
+    If the user wants a short response, you should provide a short response. 
     Your task is to:
 
     Interpret the email context to extract relevant information.
