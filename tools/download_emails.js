d19432495dae749961b393505306c17ad0f5b4e1
abhaysudhir <abhaysudhir@icloud.com>
2025-03-26 21:58:05 -0700

    untested download_emails.js file with start and end date functionality

--- tools/download_emails.js
+++ tools/download_emails.js
@@ -1,19 +1,38 @@
+// Configuration variables
+var CONFIG = {
+  groupEmail: 'MissionPeakTroop125@googlegroups.com',
+  startDate: new Date('2024-01-01'), // Set your desired start date here
+  endDate: new Date('2024-12-31')    // Set your desired end date here
+};
+
 function saveEmailsAsEML() {
-  var groupEmail = 'MissionPeakTroop125@googlegroups.com'; // Your group email
-  var threads = GmailApp.search('to:' + groupEmail); // Searches all emails sent to the group
-  var folder = DriveApp.createFolder('MissionPeakTroop125_Emails_2024_2025'); // Creates a folder in Google Drive to store the EML files
+  var groupEmail = CONFIG.groupEmail;
+  
+  // Format dates for Gmail search query
+  var startDateStr = Utilities.formatDate(CONFIG.startDate, Session.getScriptTimeZone(), "yyyy/MM/dd");
+  var endDateStr = Utilities.formatDate(CONFIG.endDate, Session.getScriptTimeZone(), "yyyy/MM/dd");
+  
+  // Create search query with date range
+  var searchQuery = 'to:' + groupEmail + ' after:' + startDateStr + ' before:' + endDateStr;
+  var threads = GmailApp.search(searchQuery);
+  
+  // Create folder with date range in name
+  var folderName = 'MissionPeakTroop125_Emails_' + 
+                   Utilities.formatDate(CONFIG.startDate, Session.getScriptTimeZone(), "yyyy_MM") + '_' +
+                   Utilities.formatDate(CONFIG.endDate, Session.getScriptTimeZone(), "yyyy_MM");
+  var folder = DriveApp.createFolder(folderName);
 
   for (var i = 0; i < threads.length; i++) {
     var threadMessages = threads[i].getMessages();
     for (var j = 0; j < threadMessages.length; j++) {
       var message = threadMessages[j];
-      var messageDate = message.getDate(); // Get the date of the email
+      var messageDate = message.getDate();
       
-      // Check if the email is from 2024 or 2025
-      if (messageDate.getFullYear() === 2024 || messageDate.getFullYear() === 2025) {
+      // Check if the email is within the specified date range
+      if (messageDate >= CONFIG.startDate && messageDate <= CONFIG.endDate) {
         // Convert each message to EML format
         var rawMessage = message.getRawContent();
-        var fileName = 'Message_' + messageDate.toISOString() + '.eml'; // Name the file based on the date of the email
+        var fileName = 'Message_' + messageDate.toISOString() + '.eml';
 
         // Save the email as an EML file in Google Drive
         folder.createFile(fileName, rawMessage, MimeType.PLAIN_TEXT);
